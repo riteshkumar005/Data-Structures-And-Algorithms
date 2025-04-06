6bfefac66e606a9399f62efc7065cf62cea3ce4e
Ritesh Kumar <riteshkumarara2003@gmail.com>
2025-04-06 20:04:14 +0530

    first commit

--- Searching&Sorting/Searching/SearchInRotatedSortedarray.cpp
+++ Searching&Sorting/Searching/SearchInRotatedSortedarray.cpp
@@ -3,40 +3,33 @@
 
 using namespace std;
 
-int  getPivot(vector<int>& nums){
-    int start = 0, end = nums.size()-1;
-    int mid = start + (end - start)/2;
-    while(start < end){
-        if(nums[mid] >= nums[0]){
+int getPivot(vector<int>& nums) {
+    int start = 0, end = nums.size() - 1;
+    int mid = start + (end - start) / 2;
+    while(start < end) {
+        if(nums[mid] >= nums[0]) {
             start = mid + 1;
+        } else {
+            end = mid;
         }
-        else {
-           end = mid;
-        }
-        mid = start + (end - start)/2;
+        mid = start + (end - start) / 2;
     }
     return start;
 }
 
-bool searchInRotatedSortedArray(vector<int>& nums, int k, int pivot){
-    int start = 0, end = nums.size()-1;
-
-    int mid  = start + (end - start)/2;
-    while(start <= end){
-        if(nums[mid] == k){
+bool searchInRotatedSortedArray(vector<int>& nums, int start, int end, int k) {
+    int mid;
+    while(start <= end) {
+        mid = start + (end - start) / 2;
+        if(nums[mid] == k) {
             return true;
-        }
-        else if(nums[0] <= nums[mid]){
+        } else if(nums[mid] < k) {
             start = mid + 1;
+        } else {
+            end = mid - 1;
         }
-        else{
-            end = mid ;
-        }
-        mid  = start + (end - start)/2;
-
     }
     return false;
-
 }
 
 int main() {
@@ -44,28 +37,39 @@ int main() {
     int size;
     cout << "Enter the size of the array: ";
     cin >> size;
-    int search_element ;
-    cout <<"Enter  elements in array: ";
+
+    cout << "Enter elements in array: ";
     for(int i = 0; i < size; i++) {
         int temp;
         cin >> temp;
         nums.push_back(temp);
     }
+
     cout << "Original array: ";
     for(int num : nums) {
         cout << num << " ";
     }
     cout << endl;
+
+    int search_element;
     cout << "Enter the element to search: ";
     cin >> search_element;
 
-    cout <<"Pivot element";
     int pivot = getPivot(nums);
+    cout << "Pivot element index: " << pivot << endl;
 
-    int PeakIndex = searchInRotatedSortedArray(nums, search_element, pivot);
-    cout << "Peak index: " << PeakIndex << endl;
+    bool found;
+    if(search_element >= nums[pivot] && search_element <= nums[size - 1]) {
+        found = searchInRotatedSortedArray(nums, pivot, size - 1, search_element);
+    } else {
+        found = searchInRotatedSortedArray(nums, 0, pivot - 1, search_element);
+    }
 
-
+    if(found) {
+        cout << "Element found in the array." << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
 
     return 0;
 }
